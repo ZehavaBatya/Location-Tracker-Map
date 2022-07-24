b60ba48149053362c41d696ef3d2d5097eab8e3f
Annaliese B <84485729+ZehavaBatya@users.noreply.github.com>
2022-07-23 21:20:12 -0400

    V1.2 (1 marker, colored map, clickable marker)

---
+++ Location Tracker Map (Version 1.1)/script.js
@@ -0,0 +1,31 @@
+function initMap(){
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 35.7426369589774, lng: -86.87656531479203},
+        zoom: 5,
+        mapId: '7db51f2b3fa06609'
+    });
+
+    const marker = new google.maps.Marker({
+        position: { lat: 41.95593944635574, lng: -87.56869780981485 },
+        map,
+        title: "Hello World!",
+        animation: google.maps.Animation.DROP
+    });
+
+    const infowindow = new google.maps.InfoWindow({
+        content: "Bob Saget",
+    });
+
+    marker.addListener("click", () => {
+        infowindow.open(map, marker);
+    });
+
+
+}
+
+//Tennessee latitude and longitutde
+//35.74263695897742, -86.87656531479203
+
+//Chicago latittude and longitude
+//41.95593944635574, -87.56869780981485
+
